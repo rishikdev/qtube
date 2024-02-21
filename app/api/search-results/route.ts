189d2f5e3d9eea5b5f1bef7a45b31b526baafc17
RishikDevTerminal <82426895+rishikdev@users.noreply.github.com>
2024-02-21 09:39:27 -0600

    update route.ts | fix bugs

--- app/api/search-results/route.ts
+++ app/api/search-results/route.ts
@@ -5,16 +5,16 @@ export async function POST(request: Request) {
   const body = await request.json();
   var parameters: string = "";
 
-  parameters = `&q=${body.searchQuery}`;
+  parameters = `q=${body.searchQuery}`;
 
-  if (body.nextPageToken != undefined) {
+  if (body.nextPageToken != undefined && body.nextPageToken != "") {
     parameters = parameters + `&pageToken=${body.nextPageToken}`;
   }
-  const apiUrl = `${process.env.YOUTUBE_SEARCH_API}${parameters}`;
-
+  const apiUrl = `${process.env.YOUTUBE_SEARCH_API}&${parameters}&key=${process.env.YOUTUBE_API_KEY}`;
   try {
     const response = await fetch(apiUrl);
     const data = (await response.json()) as YTSearchResponse;
+    console.log("data:", data);
     return new Response(
       JSON.stringify({
         items: data.items,
@@ -22,6 +22,7 @@ export async function POST(request: Request) {
       })
     );
   } catch (error) {
+    console.log("error:", error);
     return new Response(JSON.stringify({ error: error }));
   }
 }
--- app/api/video/route.ts
+++ app/api/video/route.ts
@@ -3,17 +3,19 @@ import { YTVideo, YTVideoResponse } from "@/app/yt-video-types";
 export async function POST(request: Request) {
   const body = await request.json();
 
-  const apiUrl = `${process.env.YOUTUBE_VIDEO_API}&id=${body.videoId}`;
+  const apiUrl = `${process.env.YOUTUBE_VIDEO_API}&id=${body.videoId}&key=${process.env.YOUTUBE_API_KEY}`;
 
   try {
     const response = await fetch(apiUrl);
     const data = (await response.json()) as YTVideoResponse;
+    console.log("data:", data);
     return new Response(
       JSON.stringify({
         video: data.items[0],
       })
     );
   } catch (error) {
+    console.log("error:", error);
     return new Response(JSON.stringify({ error: error }));
   }
 }
